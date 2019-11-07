f2c3b3971e69c2876dda7ccb458d3fe4970f25a3
gabber78 <gabor.rab@gmail.com>
2019-11-07 17:05:47 +0100

    edit.php changes

--- www/edit.php
+++ www/edit.php
@@ -16,11 +16,19 @@ require_once '../db.php';
  ?>
 
  <h3>Edit employee</h3>
- <form class="" action="edit.php" method="post">
-   <input type="text" name="id" placeholder="Id">
-   <input type="text" name="name" placeholder="Name">
-   <!--<input type="text" name="position" placeholder="Position">
-   <input type="text" name="email" placeholder="email">
-   <input type="date" name="startDate" placeholder="Start Date"> -->
-   <input type="submit" name="editData" value="Submit Changes">
- </form>
+   <form class="" action="edit.php" method="post">
+       <!-- do not show the edit fields until no search results, $_GET data coming from search.php because search.php included -->
+       <?php if (isset($_GET['search'])) {
+         foreach($data as $row) ?>
+
+             <input type="text" name="id" value="<?php echo $row['id'];?>" placeholder="Id">
+             <input type="text" name="name" placeholder="Name" value="<?php echo $row['name'];?>">
+             <input type="text" name="position" placeholder="Position" value="<?php echo $row['position'];?>">
+             <input type="text" name="email" placeholder="email" value="<?php echo $row['email'];?>">
+             <input type="date" name="startDate" placeholder="Start Date" value="<?php echo $row['startDate'];?>">
+
+             <input type="submit" name="editData" value="Submit Changes">
+
+       <?php }
+          ?>
+   </form>
